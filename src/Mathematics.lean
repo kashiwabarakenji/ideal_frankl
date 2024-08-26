824d676e3729bfc6c9fe6ff58eff83fb7d30ac31
kashiwa <kashiwa@idea.c.u-tokyo.ac.jp>
2024-08-26 09:24:30 +0900

    rikadai

---
+++ src/Mathematics.lean
@@ -0,0 +1,10 @@
+--import Mathematics.BasicDefinitions
+--import Mathematics.BasicLemmas
+import Mathematics.IdealTrace
+import Mathematics.IdealDeletion
+
+--export Mathematics.BasicDefinitions (*)
+--export Mathematics.BasicLemmas (*)
+export Mathematics.IdealTrace (trace)
+export Mathematics.IdealDeletion (deletion)
+export Mathematics.IdealDeletion (contraction)
