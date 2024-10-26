15e9770f5bf93368004508e94e2f06bcd86155b6
kashiwa <kashiwa@idea.c.u-tokyo.ac.jp>
2024-10-26 19:24:22 +0900

    readme作成

--- src/Ideal/IdealMain.lean
+++ src/Ideal/IdealMain.lean
@@ -1,5 +1,5 @@
---Ideal集合族が平均abundantにならないことの証明が目標。まだうまく行ってないが保留にする。
-import Mathlib.Data.Finset.Basic --hiding eq_empty_of_subset_empty
+--Ideal集合族が平均abundantにならないことの証明。メイン定理。
+import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Finset.Lattice
 import Mathlib.Data.Finset.Card
 import Mathlib.Data.Fintype.Basic
@@ -26,55 +26,12 @@ namespace Ideal
 
 variable {α : Type} [DecidableEq α] [Fintype α] [Nonempty α]
 
-omit [Fintype α] [Nonempty α]
-
-theorem filter_union_distrib (P : α → Prop) [DecidablePred P] (A B : Finset α) :
-  Finset.filter P (A ∪ B)= (A.filter P) ∪ (B.filter P) := by
-  -- 両方の集合の同値性を示すために ext を使う
-  ext x
-  -- x が (A ∪ B).filter P に含まれる ⇔ A.filter P または B.filter P に含まれる
-  simp [Finset.mem_union, Finset.mem_filter]
-  -- x が A ∪ B のどちらかに属し、かつ P(x) を満たすかどうかを確認
-  tauto -- 両方の論理条件を自動的に解決
-
-lemma filter_union_eq (P : Finset α → Prop) [DecidablePred P] (A B : Finset (Finset α)) : (A ∪ B).filter P = (A.filter P) ∪ (B.filter P) := filter_union_distrib P A B
---Finset.filter P
-lemma filter_union_eq0  (P : α → Prop) [DecidablePred P] (A B: Finset α) : Finset.filter P (A ∪ B) =  (Finset.filter P B) ∪ (Finset.filter P A)  := by
-  ext x
-  -- フィルタリングされた要素について、それがA∪Bに属するかを確認
-  simp [Finset.mem_filter, Finset.mem_union]
-  -- 両辺を示すための同値性
-  tauto
-
-
-theorem filter_union_sum (P : Finset α → Prop) [DecidablePred P] (A B : Finset (Finset α)) (disj: Disjoint A  B) :
-  ((Finset.filter P A).sum (λ x => x.card)) + ((Finset.filter P B).sum (λ x => x.card)) = (Finset.filter P (A ∪ B)).sum (λ x => x.card) := by
-
-
-
-   -- A と B が互いに素であることから、フィルタ後の A.filter P と B.filter P も互いに素
-   have filter_disj : Disjoint (A.filter P) (B.filter P) := by
-      --rw [Finset.disjoint_iff_inter_eq_empty]
-      --have disjAB : Disjoint A B := by
-      --  rw [Finset.disjoint_iff_inter_eq_empty]
-      --  exact disj
-      exact Finset.disjoint_filter_filter disj
-   --have sum_disjoint := Finset.sum (A.filter P ∪ B.filter P) (λ x => x.card)
-   have sum_disjoint := (@Finset.sum_union _ _ (A.filter P) (B.filter P)  (λ x => x.card)) filter_disj
-   rw [←sum_disjoint]
-   rw [←filter_union_eq]
-
---disjoint_filter_filterにあった。
---theorem filter_disjoint (P : Finset α → Prop) [DecidablePred P] (A B : Finset (Finset α)) (disj: Disjoint A  B):
---  Disjoint (Finset.filter P A) (Finset.filter P B) := by
-
---def P (x:Nat) : Prop := x ≥ 2  ∧ ∀ (F: IdealFamily (Fin x)), F.ground.card = x → normalized_degree_sum F.toSetFamily ≤ 0
-
+--メイン定理の帰納法のベースケース。大きさ2の場合の証明。
 theorem basecase : P 2 := by
   constructor
   simp_all only [ge_iff_le]
   simp_all only [le_refl]
-  intros F hcard
+  intros F ground_card
   dsimp [normalized_degree_sum]
   simp_all only [ge_iff_le, tsub_le_iff_right, zero_add]
   dsimp [total_size_of_hyperedges]
@@ -164,13 +121,10 @@ theorem basecase : P 2 := by
     simp_all only [Fin.isValue, Finset.powerset_univ, Finset.disjoint_insert_right, Finset.mem_insert,
       Finset.mem_singleton, Finset.singleton_ne_empty, or_false, Finset.disjoint_singleton_right, one_ne_zero,
       not_false_eq_true, Finset.union_insert, Finset.insert_union, zeroone]
-    --obtain ⟨left, right⟩ := zeroonedisj
     rfl
 
   have leftside: ∑ x in Finset.filter F.sets (Finset.powerset zeroone), x.card = 2 + (Finset.card (Finset.filter (λ x => F.sets {x}) zeroone)) := by
     rw [pow2]
-    --dsimp [zeroone]
-    --rw [Finset.filter]
     let dist:= filter_union_distrib F.sets ({{0,1},∅}) ({{0},{1}})
     simp at dist
 
@@ -178,9 +132,6 @@ theorem basecase : P 2 := by
     let filtereq := filter_union_eq F.sets ({{0,1},∅}) ({{0},{1}}) --うまくいってない？{{0, 1}, ∅} ∪ {{0}, {1}}= {{0, 1}, {0}, {1}, ∅} が成り立つはず。
     simp at filtereq
     rw [zerooneunion] at distsum
-    --rw [filtereq] at dist
-    --simp
-    --rw [←dist]
     rw [←distsum]
 
     have goal1:  ∑ x ∈ Finset.filter F.sets {{0, 1}, ∅}, x.card = 2 := by
@@ -190,10 +141,6 @@ theorem basecase : P 2 := by
         Finset.singleton_ne_empty, or_false, Finset.disjoint_singleton_right, Finset.union_insert,
         Finset.insert_union]
       obtain ⟨left, right⟩ := zeroonedisj
-      symm
-      symm
-      symm
-      symm
       simp [Finset.filter_true_of_mem, *]
       simp_all only [Fin.isValue, one_ne_zero, not_false_eq_true, Finset.image_insert, Finset.image_singleton,
         Finset.image_empty, Finset.disjoint_empty_right, Finset.union_assoc, Finset.mem_union, Finset.mem_singleton,
@@ -226,7 +173,6 @@ theorem basecase : P 2 := by
           simp_all only [Fin.isValue, Finset.powerset_univ, Finset.disjoint_insert_right, Finset.mem_insert,
             Finset.mem_singleton, Finset.singleton_ne_empty, or_false, Finset.disjoint_singleton_right,
             Finset.union_insert, Finset.insert_union, ↓reduceIte, zeroone]
-          --obtain ⟨left, right⟩ := zeroonedisj
           symm
           simp_all only [Fin.isValue, Finset.card_eq_zero]
           ext1 a
@@ -240,7 +186,6 @@ theorem basecase : P 2 := by
           simp_all only [Fin.isValue, Finset.powerset_univ, Finset.disjoint_insert_right, Finset.mem_insert,
             Finset.mem_singleton, Finset.singleton_ne_empty, or_false, Finset.disjoint_singleton_right,
             Finset.union_insert, Finset.insert_union, ↓reduceIte, zeroone]
-          --obtain ⟨left, right⟩ := zeroonedisj
           symm
           simp [result0, Finset.filter_singleton]
           simp_all only [Fin.isValue, ↓reduceIte, Finset.card_singleton]
@@ -248,7 +193,6 @@ theorem basecase : P 2 := by
           simp_all only [Fin.isValue, Finset.powerset_univ, Finset.disjoint_insert_right, Finset.mem_insert,
             Finset.mem_singleton, Finset.singleton_ne_empty, or_false, Finset.disjoint_singleton_right,
             Finset.union_insert, Finset.insert_union, ↓reduceIte, zeroone]
-          --obtain ⟨left, right⟩ := zeroonedisj
           symm
           simp_all only [Fin.isValue, Finset.card_eq_zero]
           ext1 a
@@ -401,36 +345,34 @@ theorem basecase : P 2 := by
   rw [new_goal]
 
   --leftsideとrightsideを使って、goalを証明する。
-
-
-theorem induction_step {n:Nat} (hn: n >= 2) (h_ind: P n) : P (n+1) := by
+theorem induction_step {n:Nat} (n_ge_two: n >= 2) (h_ind: P n) : P (n+1) := by
   -- ここでFintypeインスタンスを明示的に使用
   --have fintype_ground : Fintype F.ground := finF
-  have nposi : n ≥ 1 := by omega
+  have n_ge_one : n ≥ 1 := by omega
   unfold P at h_ind ⊢
-  obtain ⟨h_ge2, h_ind⟩ := h_ind
+  obtain ⟨n_ge_two, h_ind⟩ := h_ind
 
   constructor
   simp_all only [ge_iff_le, Nat.reduceLeDiff]
   --obtain ⟨left, right⟩ := h
   --omega
-  intros F hcard
+  intros F ground_card
    -- n ≥ 2 から n + 1 ≥ 3 を導く
-  have hcard0: F.ground.card >= 2 := by
-    have h1 : n + 1 ≥ 3 := Nat.succ_le_succ hn
+  have ground_ge_two: F.ground.card >= 2 := by
+    have h1 : n + 1 ≥ 3 := Nat.succ_le_succ n_ge_two
     -- F.ground.card = n + 1 なので、F.ground.card ≥ 3
-    rw [←hcard] at h1
+    rw [←ground_card] at h1
     -- F.ground.card ≥ 3 なので F.ground.card ≥ 2 も成立
     exact Nat.le_of_succ_le h1
 
   obtain ⟨v, hv⟩ := ideal_version_of_frankl_conjecture F
     --#check hv
-  obtain ⟨hv_left, hv_right⟩ := hv
+  obtain ⟨v_in_ground, hv_right⟩ := hv
 
-  by_cases hv_singleton: F.sets {v}
+  by_cases singleton_hyperedge_have: F.sets {v}
   · case pos =>
 
-    set Fdel := IdealDeletion.idealdeletion F v hv_left hcard0
+    set Fdel := IdealDeletion.idealdeletion F v v_in_ground ground_ge_two
     have Fvx: v ∉ Fdel.ground := by
       intro h
       simp_all only [ge_iff_le]
@@ -439,32 +381,32 @@ theorem induction_step {n:Nat} (hn: n >= 2) (h_ind: P n) : P (n+1) := by
       dsimp [IdealDeletion.idealdeletion] at h
       simp_all only [Finset.mem_erase, ne_eq, not_true_eq_false, and_true]
 
-    have hcard1: Fdel.ground.card = n := by
+    have ground_Fdel_card: Fdel.ground.card = n := by
       simp_all only [ge_iff_le]
       simp_all only [Fdel]
       rw [IdealDeletion.idealdeletion]
       simp_all only [Finset.card_erase_of_mem, add_tsub_cancel_right]
 
-    have hcard2: Fdel.ground.card ≥ 1 := by
+    have ground_delF_card_ge_one: Fdel.ground.card ≥ 1 := by
       simp_all only [ge_iff_le]
 
-    --#check IdealFamily.deletionToN nposi Fdel v Fvx hcard2
-    set h_idealdeletion := IdealFamily.deletionToN nposi Fdel v Fvx hcard2
+    --#check IdealFamily.deletionToN n_ge_one Fdel v Fvx ground_delF_card_ge_one
+    set h_idealdeletion := IdealFamily.deletionToN n_ge_one Fdel v Fvx ground_delF_card_ge_one
     --IdealFamily (Fin (n + 1))になっているがFin nになってほしい。
 
-    have hcard3: h_idealdeletion.ground.card = n := by
+    have ground_delF_card: h_idealdeletion.ground.card = n := by
       dsimp [h_idealdeletion]
       dsimp [IdealFamily.deletionToN]
-      rw  [finDropCardEq nposi v Fdel.ground Fvx]
-      exact hcard1
+      rw  [finDropCardEq n_ge_one v Fdel.ground Fvx]
+      exact ground_Fdel_card
 
-    have h_del_card: (@IdealFamily.deletionToN (Fin n) n nposi (IdealDeletion.idealdeletion F v hv_left hcard0) v Fvx
-                hcard2).ground.card = n := by
+    have h_del_card: (@IdealFamily.deletionToN (Fin n) n n_ge_one (IdealDeletion.idealdeletion F v v_in_ground ground_ge_two) v Fvx
+                ground_delF_card_ge_one).ground.card = n := by
       simp_all only [ge_iff_le]
       simp_all only [ge_iff_le, implies_true, imp_self, forall_true_left, Fdel, h_idealdeletion]
-      exact hcard3
+      exact ground_delF_card
 
-    set Fcont :=  (IdealDeletion.contraction_ideal_family F v hv_singleton hcard0)
+    set Fcont :=  (IdealDeletion.contraction_ideal_family F v singleton_hyperedge_have ground_ge_two)
     have h_cont: Fcont.ground.card = n := by
       simp_all only [ge_iff_le]
       --simp_all only [IdealDeletion.contraction]
@@ -489,20 +431,20 @@ theorem induction_step {n:Nat} (hn: n >= 2) (h_ind: P n) : P (n+1) := by
     have h_cont_card: Fcont.ground.card = n := by
       simp_all only [ge_iff_le]
 
-    have h_cont_card2: (IdealDeletion.contraction_ideal_family F v hv_singleton hcard0).ground.card = n:= by
+    have h_cont_card2: (IdealDeletion.contraction_ideal_family F v singleton_hyperedge_have ground_ge_two).ground.card = n:= by
       simp_all only [ge_iff_le]
 
-    set h_contraction := IdealFamily.deletionToN nposi Fcont v Fvx2 h_cont2
+    set h_contraction := IdealFamily.deletionToN n_ge_one Fcont v Fvx2 h_cont2
     have h_cont_card3: h_contraction.ground.card = n := by
       simp_all only [ge_iff_le]
       dsimp [h_contraction]
       dsimp [IdealFamily.deletionToN]
-      rw [finDropCardEq nposi v Fcont.ground Fvx2]
+      rw [finDropCardEq n_ge_one v Fcont.ground Fvx2]
       exact h_cont_card
 
-    dsimp [Fdel] at hcard1
-    --#check (h_ind h_idealdeletion) hcard3
-    let h_idealdeletion2 := h_ind h_idealdeletion hcard3
+    dsimp [Fdel] at ground_Fdel_card
+    --#check (h_ind h_idealdeletion) ground_delF_card
+    let h_idealdeletion2 := h_ind h_idealdeletion ground_delF_card
     --#check h_ind h_contraction
     let h_contraction2 := (h_ind h_contraction) h_cont_card3
 
@@ -515,8 +457,8 @@ theorem induction_step {n:Nat} (hn: n >= 2) (h_ind: P n) : P (n+1) := by
     simp only [ge_iff_le, tsub_le_iff_right, zero_add, Fdel, Fcont] at h_idealdeletion2 h_contraction2  ⊢
     simp only [normalized_degree_sum] at h_idealdeletion2 h_contraction2  ⊢
 
-    rw [IdealFamily.deletionToN_number nposi Fdel v Fvx hcard2] at h_idealdeletion2
-    rw [IdealFamily.deletionToN_number nposi Fcont v Fvx2 h_cont2] at h_contraction2
+    rw [IdealFamily.deletionToN_number n_ge_one Fdel v Fvx ground_delF_card_ge_one] at h_idealdeletion2
+    rw [IdealFamily.deletionToN_number n_ge_one Fcont v Fvx2 h_cont2] at h_contraction2
     dsimp [h_idealdeletion] at h_idealdeletion2
     dsimp [h_contraction] at h_contraction2
     rw [deletion_total] at h_idealdeletion2 h_contraction2
@@ -524,17 +466,16 @@ theorem induction_step {n:Nat} (hn: n >= 2) (h_ind: P n) : P (n+1) := by
     dsimp [Fcont] at h_contraction2
     rw [h_del_card] at h_idealdeletion2
 
-    --以下の部分も場合分けの前に移動したほうがよいかも。
-    --let total_del := (total_size_of_hyperedges ((@IdealFamily.deletionToN (Fin n) n nposi Fdel v Fvx hcard2):IdealFamily (Fin n)).1)
-    set total_del := total_size_of_hyperedges (IdealDeletion.idealdeletion F v hv_left hcard0).1
-    --set number_del := (number_of_hyperedges (@IdealFamily.deletionToN (Fin n) n nposi Fdel v Fvx hcard2).1) with number_del
-    set number_del := (number_of_hyperedges (IdealDeletion.idealdeletion F v hv_left hcard0).1)
-    --let total_cont := (total_size_of_hyperedges (@IdealFamily.deletionToN (Fin n) n nposi Fcont v Fvx2 h_cont2).1)
-    --set total_cont := total_size_of_hyperedges (IdealDeletion.contraction F.1 v hv_left hcard0) with h_total_cont
-    set total_cont := total_size_of_hyperedges (IdealDeletion.contraction_ideal_family F v hv_singleton hcard0).toSetFamily
-    --let number_cont := (number_of_hyperedges (@IdealFamily.deletionToN (Fin n) n nposi Fcont v Fvx2 h_cont2).1)
-    --set number_cont := (number_of_hyperedges (IdealDeletion.contraction F.1 v hv_left hcard0)) with h_number_cont
-    set number_cont := (number_of_hyperedges (IdealDeletion.contraction_ideal_family F v hv_singleton hcard0).toSetFamily)
+    --let total_del := (total_size_of_hyperedges ((@IdealFamily.deletionToN (Fin n) n n_ge_one Fdel v Fvx ground_delF_card_ge_one):IdealFamily (Fin n)).1)
+    set total_del := total_size_of_hyperedges (IdealDeletion.idealdeletion F v v_in_ground ground_ge_two).1
+    --set number_del := (number_of_hyperedges (@IdealFamily.deletionToN (Fin n) n n_ge_one Fdel v Fvx ground_delF_card_ge_one).1) with number_del
+    set number_del := (number_of_hyperedges (IdealDeletion.idealdeletion F v v_in_ground ground_ge_two).1)
+    --let total_cont := (total_size_of_hyperedges (@IdealFamily.deletionToN (Fin n) n n_ge_one Fcont v Fvx2 h_cont2).1)
+    --set total_cont := total_size_of_hyperedges (IdealDeletion.contraction F.1 v v_in_ground ground_ge_two) with h_total_cont
+    set total_cont := total_size_of_hyperedges (IdealDeletion.contraction_ideal_family F v singleton_hyperedge_have ground_ge_two).toSetFamily
+    --let number_cont := (number_of_hyperedges (@IdealFamily.deletionToN (Fin n) n n_ge_one Fcont v Fvx2 h_cont2).1)
+    --set number_cont := (number_of_hyperedges (IdealDeletion.contraction F.1 v v_in_ground ground_ge_two)) with h_number_cont
+    set number_cont := (number_of_hyperedges (IdealDeletion.contraction_ideal_family F v singleton_hyperedge_have ground_ge_two).toSetFamily)
     set total := (total_size_of_hyperedges F.toSetFamily)
     set  number := (number_of_hyperedges F.toSetFamily)
     set degreev := (degree F.toSetFamily v)
@@ -542,10 +483,10 @@ theorem induction_step {n:Nat} (hn: n >= 2) (h_ind: P n) : P (n+1) := by
     classical
     by_cases hv_hyperedge:(F.sets (F.ground \ {v}))
     ·   case pos =>
-        have h_sum_have := (hyperedge_average_have F v hv_left hcard0) hv_hyperedge hv_singleton
-        --have h_idealdeletion := (IdealDeletion.idealdeletion F v hv_left hcard0)
+        have h_sum_have := (hyperedge_average_have F v v_in_ground ground_ge_two) hv_hyperedge singleton_hyperedge_have
+        --have h_idealdeletion := (IdealDeletion.idealdeletion F v v_in_ground ground_ge_two)
         --#check sum_have
-        let number_have :=  hyperedge_count_deletion_contraction_have_z F v hv_left hcard0 hv_hyperedge hv_singleton
+        let number_have :=  hyperedge_count_deletion_contraction_have_z F v v_in_ground ground_ge_two hv_hyperedge singleton_hyperedge_have
 
         simp only [ge_iff_le, tsub_le_iff_right, zero_add, Fdel, Fcont] at h_sum_have number_have
         simp only [normalized_degree_sum] at h_sum_have number_have
@@ -556,9 +497,9 @@ theorem induction_step {n:Nat} (hn: n >= 2) (h_ind: P n) : P (n+1) := by
         linarith
 
     ·   case neg =>
-        --hv_hyperedge:(F.sets (F.ground \ {v}))が成り立たないケース。hv_singleton : ¬F.sets {v}のケースかも。どちらも未解決。
-        have h_sum_none := hyperedge_average_none F v hv_left hcard0 hv_hyperedge hv_singleton
-        have number_none := hyperedge_count_deletion_contraction_none F v hv_left hcard0 hv_hyperedge hv_singleton
+        --hv_hyperedge:(F.sets (F.ground \ {v}))が成り立たないケース。singleton_hyperedge_have : ¬F.sets {v}のケースかも。どちらも未解決。
+        have h_sum_none := hyperedge_average_none F v v_in_ground ground_ge_two hv_hyperedge singleton_hyperedge_have
+        have number_none := hyperedge_count_deletion_contraction_none F v v_in_ground ground_ge_two hv_hyperedge singleton_hyperedge_have
 
         simp only [ge_iff_le, tsub_le_iff_right, zero_add, Fdel, Fcont] at h_sum_none number_none
         simp only [normalized_degree_sum] at h_sum_none number_none
@@ -566,14 +507,14 @@ theorem induction_step {n:Nat} (hn: n >= 2) (h_ind: P n) : P (n+1) := by
         Fdel, Fcont, h_contraction, total_del, number_del, total_cont, number_cont, total]
         linarith
 
-  --case negがもう一つある。hv_singleton:(F.sets {v})が成り立たないケース。
-  --hv_singleton:(F.sets {v})が成り立たないケース。tabでインデントを調整して見えるようになった。
+  --case negがもう一つある。singleton_hyperedge_have:(F.sets {v})が成り立たないケース。
+  --singleton_hyperedge_have:(F.sets {v})が成り立たないケース。tabでインデントを調整して見えるようになった。
   · case neg =>
 
         have h_indPn : P n := by
           unfold P
-          exact ⟨h_ge2, h_ind⟩
-        exact degonemain n F v hv_left hv_singleton hcard0 hcard h_indPn
+          exact ⟨n_ge_two, h_ind⟩
+        exact degonemain n F v v_in_ground singleton_hyperedge_have ground_ge_two ground_card h_indPn
 
 --ideal集合族が平均rareであることの定理。今回のプロジェクトのメイン定理。
 theorem P_all (x : Nat) (hx : x ≥ 2) : P x := by
@@ -589,5 +530,5 @@ theorem P_all (x : Nat) (hx : x ≥ 2) : P x := by
       | zero => exact basecase -- x = 2 の場合は basecase を適用
       | succ m =>
         -- x = m + 3 以上の場合
-        have hn : m + 2 ≥ 2 := Nat.le_add_left _ _ -- m + 2 は 2 以上
-        exact induction_step hn (P_all (m + 2) hn)
+        have m_ge_two : m + 2 ≥ 2 := Nat.le_add_left _ _ -- m + 2 は 2 以上
+        exact induction_step m_ge_two (P_all (m + 2) m_ge_two)
