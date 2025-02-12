263eabe4f7ec12c3f6cd2b92fdcfe01c84280bff
kashiwa <kashiwa@idea.c.u-tokyo.ac.jp>
2025-02-12 10:41:19 +0900

    安部の定理の通常版の証明が完成

--- rooted/abe.lean
+++ rooted/abe.lean
@@ -21,22 +21,27 @@ variable {α : Type}  [DecidableEq α] [Fintype α]
 
 open Classical
 
+--サイズ1の根つき集合を与える関数。
 noncomputable def root_vertices_sizeone (SF: ClosureSystem α)[DecidablePred SF.sets] (s: Finset SF.ground) : Finset SF.ground :=
   let RS := rootedSetsFromSetFamily SF.toSetFamily
 (SF.ground.filter (fun x  => x ∈ (s.image Subtype.val) ∧ ∃ (r : ValidPair α), r ∈ RS.rootedsets ∧ r.root = x ∧ r.stem ⊆ s.image Subtype.val)).subtype (fun x => x ∈ SF.ground)
 
+--安部の論文に出てくるダッシュの作用素。サイズ1の根の集合のclosure。
 noncomputable def dash (SF: ClosureSystem α)[DecidablePred SF.sets]  (s: Finset SF.ground) : Finset SF.ground :=
 closureOperator SF (root_vertices_sizeone SF s )
 
+--hyperege全体が包含関係で作る束のなかで、coverの関係の定義
 noncomputable def cover (SF: ClosureSystem α)[DecidablePred SF.sets]  (A B : Finset α) : Prop := SF.sets A ∧ SF.sets B ∧ A ⊂ B ∧ (∀ C :Finset α, SF.sets C → A ⊆ C → A ⊆ B → C = A ∨ C = B)
 
 -- `B` をカバーする `A` すべての共通部分を求める関数 -/
 noncomputable def coveringIntersection {α : Type} [DecidableEq α] [Fintype α] (SF: ClosureSystem α)[DecidablePred SF.sets] (B : Finset α): Finset α :=
   finsetIntersection ((SF.ground.powerset.filter (fun s => (SF.sets s ∧ (cover SF s B)) ∨ s = B)))
 
+-- coveringIntersectionの引数として、subtypeを取れるようにしたもの。値もsubtype
 noncomputable def coveringIntersection_sub {α : Type} [DecidableEq α] [Fintype α]  [Fintype α] (SF: ClosureSystem α) [DecidablePred SF.sets] (B : Finset SF.ground): Finset SF.ground :=
   (@coveringIntersection α _ _ SF _ (B.image Subtype.val)).subtype (fun x => x ∈ SF.ground)
 
+--Xに含まれるsize 1の根つき集合の根の集合 root_vertices_sizeone Xは、包含関係に関して単調。
 lemma monotone_root_vertices_sizeone (SF: ClosureSystem α)[DecidablePred SF.sets]  (A B : Finset SF.ground):A ⊆ B → root_vertices_sizeone SF A ⊆ root_vertices_sizeone SF B:=
 by
   intro hab
@@ -115,13 +120,12 @@ by
   `X1 = X2` となることを示す。
 -/
 lemma injective_cover (SF : ClosureSystem α) [DecidablePred SF.sets]
-    (X1 X2 Y1 Y2 : Finset α)
-    (c1 : cover SF Y1 X1) (c2 : cover SF Y2 X2)
-    (hY : Y1 = Y2)
-    (h_nonempty : ((X1 \ Y1) ∩ (X2 \ Y2)).Nonempty) : X1 = X2 := by
+    (X1 X2 Y : Finset α)
+    (c1 : cover SF Y X1) (c2 : cover SF Y X2)
+    (h_nonempty : ((X1 \ Y) ∩ (X2 \ Y)).Nonempty) : X1 = X2 :=
+by
 
   /- まず `Y1 = Y2` を書き換え -/
-  rw [←hY] at c2
 
   /- `c1`, `c2` から被覆の成分を取り出す -/
   obtain ⟨hY1, hX1, hY1_ss_X1, cover_cond1⟩ := c1
@@ -131,7 +135,7 @@ lemma injective_cover (SF : ClosureSystem α) [DecidablePred SF.sets]
   have h_inter : SF.sets (X1 ∩ X2) := SF.intersection_closed X1 X2 hX1 hX2
 
   /- Y1 ⊆ X1, Y1 ⊆ X2 より Y1 ⊆ X1 ∩ X2 -/
-  have hY1_sub_inter : Y1 ⊆ X1 ∩ X2 := by
+  have hY1_sub_inter : Y ⊆ X1 ∩ X2 := by
     rw [@Finset.subset_inter_iff]
     constructor
     rw [Finset.ssubset_def] at hY1_ss_X1
@@ -142,29 +146,24 @@ lemma injective_cover (SF : ClosureSystem α) [DecidablePred SF.sets]
 
   /- X1 ∩ X2 ⊆ X1, X1 ∩ X2 ⊆ X2 は自明 -/
   have h_inter_sub_X1 : X1 ∩ X2 ⊆ X1 := by
-    subst hY
     simp_all only [Finset.inter_subset_left]
   have h_inter_sub_X2 : X1 ∩ X2 ⊆ X2 := by
-    subst hY
     simp_all only [Finset.inter_subset_left, Finset.inter_subset_right]
 
   /- (X1 \ Y1) ∩ (X2 \ Y1) が非空 ⇒ X1 ∩ X2 は Y1 と一致しないことを示す -/
   obtain ⟨x, hx⟩ := h_nonempty
   have hx_in_X1X2 : x ∈ X1 ∩ X2 := by
     -- x ∈ X1 \ Y1 および x ∈ X2 \ Y1 から x ∈ X1 ∩ X2 を取り出す
-    subst hY
     simp_all only [Finset.inter_subset_left, Finset.inter_subset_right, Finset.mem_inter, Finset.mem_sdiff, and_self]
-  have hx_not_in_Y1 : x ∉ Y1 := by
+  have hx_not_in_Y1 : x ∉ Y := by
     -- x ∈ X1 \ Y1 なので x ∉ Y1
-    subst hY
     simp_all only [Finset.inter_subset_left, Finset.inter_subset_right, Finset.mem_inter, Finset.mem_sdiff, true_and,
       and_self, not_false_eq_true]
 
   -- したがって X1 ∩ X2 は Y1 を真に含む
   -- よって X1 ∩ X2 ≠ Y1
-  have h_diff : X1 ∩ X2 ≠ Y1 := fun h_eq => by
+  have h_diff : X1 ∩ X2 ≠ Y := fun h_eq => by
     rw [← h_eq] at hx_not_in_Y1
-    subst h_eq hY
     simp_all only [Finset.inter_subset_left, Finset.inter_subset_right]
 
   /-
@@ -178,13 +177,12 @@ lemma injective_cover (SF : ClosureSystem α) [DecidablePred SF.sets]
       (3) C ⊆ X1     : h_inter_sub_X1
     を満たすので適用できる。
   -/
-  have cover_res1 : X1 ∩ X2 = Y1 ∨ X1 ∩ X2 = X1 :=
+  have cover_res1 : X1 ∩ X2 = Y ∨ X1 ∩ X2 = X1 :=
   by
     apply cover_cond1 (X1 ∩ X2) h_inter hY1_sub_inter --h_inter_sub_X1
-    subst hY
     simp_all only [Finset.inter_subset_left, Finset.inter_subset_right, Finset.mem_inter, ne_eq, Finset.mem_sdiff,
       not_false_eq_true, and_self]
-    obtain ⟨left, right⟩ := hx_in_X1X2
+    --obtain ⟨left, right⟩ := hx_in_X1X2
     intro y hy
     exact hY1_ss_X1.1 hy
 
@@ -197,7 +195,7 @@ lemma injective_cover (SF : ClosureSystem α) [DecidablePred SF.sets]
     -- X1 ∩ X2 = X1 なので X1 ⊆ X2
     have h_X1_sub_X2 : X1 ⊆ X2 := by
       rw [← eq_to_X1]
-      subst hY
+      --subst hY
       simp_all only [subset_refl, ne_eq, Finset.inter_eq_left, Finset.mem_inter, Finset.mem_sdiff, not_false_eq_true,
         and_self, and_true, true_and, Finset.inter_subset_right]
 
@@ -210,10 +208,9 @@ lemma injective_cover (SF : ClosureSystem α) [DecidablePred SF.sets]
         C ⊆ X2     : h_inter_sub_X2
       を満たすので適用可能
     -/
-    have cover_res2 : X1 ∩ X2 = Y1 ∨ X1 ∩ X2 = X2 :=
+    have cover_res2 : X1 ∩ X2 = Y ∨ X1 ∩ X2 = X2 :=
     by
       apply cover_cond2 (X1 ∩ X2) h_inter hY1_sub_inter --h_inter_sub_X2
-      subst hY
       simp_all only [subset_refl, ne_eq, Finset.inter_eq_left, Finset.mem_inter, Finset.mem_sdiff, not_false_eq_true,
         and_self, and_true, true_and, forall_const]
       exact hY1_sub_inter.trans h_X1_sub_X2
@@ -227,7 +224,6 @@ lemma injective_cover (SF : ClosureSystem α) [DecidablePred SF.sets]
       -- X1 ∩ X2 = X2 なので X2 ⊆ X1
       have h_X2_sub_X1 : X2 ⊆ X1 := by
         rw [← eq_to_X2]
-        subst hY
         simp_all only [subset_refl, ne_eq, Finset.inter_self, Finset.mem_sdiff, not_false_eq_true, and_self]
 
       -- X1 ⊆ X2 かつ X2 ⊆ X1 なので結局 X1 = X2
@@ -283,13 +279,14 @@ by
   · right
     simp
 
+--ここでのsubtypeの意味は、定義域がsubtypeでなく、値がsubtype。
+--証明方法を改善したので、結果的に使わなくなったが置いておく。
 noncomputable def maximal_hyperedge_subtype  (SF : ClosureSystem α) [DecidablePred SF.sets]
   (X : Finset α) (x : α) (SFX : SF.sets X) (hx : x ∈ X \ (coveringIntersection SF X)) :
   Subtype (λ Y => SF.sets Y ∧ x ∈ X \ Y ∧ cover SF Y X) :=
   let Y := Classical.choose (maximal_hyperedge_exists SF X x SFX hx)
   let h := Classical.choose_spec (maximal_hyperedge_exists SF X x SFX hx)
   ⟨Y, h⟩
-
 
 def plus_condition (SF:ClosureSystem α)  [DecidablePred SF.sets]:Prop:=
   ∀ X:Finset SF.ground, SF.sets (X.image Subtype.val) → (coveringIntersection_sub SF X) ⊆ (dash SF X)
@@ -391,6 +388,7 @@ by
       let nt := this notinv
       rw [Finset.mem_sdiff] at notin
       exact notin.2 nt
+  exact notin2
 
 theorem abetype_theorem (SF:ClosureSystem α)  [DecidablePred SF.sets]:
    plus_condition SF → ∀ v : α, v ∈ SF.ground \ ((dash SF SF.ground.attach).image Subtype.val) → SF.is_rare v :=
@@ -401,154 +399,130 @@ by
   let S:= SF.ground.powerset.filter (fun s => SF.sets s ∧ v ∈ s)
   let T:= SF.ground.powerset.filter (fun s => SF.sets s ∧ v ∉ s)
 
-  --sからtへの単射を作りたい。
-  let ii : S → T:= fun s =>
-    by
-      dsimp [S] at s
-      obtain ⟨val,property⟩ := s
-      let ssub:= val.subtype (fun x => x ∈ SF.ground)
-      rw [Finset.mem_filter] at property
-      rw [Finset.mem_powerset] at property
-      have sg: val ⊆ SF.ground:= property.1
-      have notin:v ∈ SF.ground \ ((dash SF ssub).image Subtype.val):=
-      by
-        rw [Finset.mem_sdiff]
-        have monod: dash SF ssub ⊆ dash SF SF.ground.attach :=
-        by
-          have : ssub ⊆ SF.ground.attach :=
-          by
-            dsimp [ssub]
-            intro x
-            intro a
-            simp_all only [Finset.mem_sdiff, Finset.mem_image, Subtype.exists, exists_and_right, exists_eq_right,
-              not_exists, true_and, Finset.mem_subtype, Finset.mem_attach, S, ssub]
-          exact monotone_dash SF ssub SF.ground.attach this
-
-        simp
-        constructor
-        · simp_all only [Finset.mem_sdiff, Finset.mem_image, Subtype.exists, exists_and_right, exists_eq_right,
-          not_exists, true_and, S, ssub]
-        · rw [Finset.mem_sdiff] at hv
-          intro hvv
-          intro vin
-          let vm := monod vin
-          simp_all only [Finset.mem_image, Subtype.exists, exists_and_right, exists_eq_right, exists_const,
-            not_true_eq_false, and_false, vm]
-          --ここまででnotinを証明。
-
-      have :(coveringIntersection SF val) ⊆ ((dash SF ssub).image Subtype.val) :=
-      by
-        dsimp [ssub]
-        let pcs := pc ssub
-        have val_eq: Finset.image Subtype.val (Finset.subtype (fun x => x ∈ SF.ground) val) = val :=
-          by
-            simp_all only [forall_true_left, S, ssub]
-            ext a : 1
-            simp_all only [Finset.mem_image, Finset.mem_subtype, Subtype.exists, exists_and_left, exists_prop,
-              exists_eq_right_right, and_iff_left_iff_imp, S, ssub]
-            intro a_1
-            exact sg a_1
-        have :SF.sets (Finset.image Subtype.val ssub) :=
-        by
-          dsimp [ssub]
-          simp_all only [Finset.mem_sdiff, Finset.mem_image, Subtype.exists, exists_and_right, exists_eq_right,
-            not_exists, true_and, exists_true_left, S, ssub]
-        specialize pcs this
-        dsimp [ssub] at pcs
-        dsimp [coveringIntersection_sub] at pcs
-        rw [val_eq] at pcs
-        intro x hx
-        have :x ∈ SF.ground :=
-        by
-          dsimp [coveringIntersection] at hx
-          rw [mem_finsetIntersection_iff_of_nonempty] at hx
-          · simp_all [S, ssub]
-            obtain ⟨left, right⟩ := hv
-            obtain ⟨left_1, right_1⟩ := property
-            simp_all only [forall_true_left, subset_refl, S, ssub]
-            apply sg
-            simp_all only [true_and, or_true, S, ssub]
-
-          · use val  -- intersectionの非空性
-            rw [Finset.mem_filter]
-            constructor
-            · rw [Finset.mem_powerset]
-              simp_all only [Finset.mem_sdiff, Finset.mem_image, Subtype.exists, exists_and_right, exists_eq_right,
-                not_exists, true_and, exists_true_left, S, ssub]
-            · right
-              rfl
-
-        simp_all [S, ssub]
-        simp_all only [S, ssub]
-        obtain ⟨left, right⟩ := hv
-        obtain ⟨left_1, right_1⟩ := property
-        simp_all only [forall_true_left, S, ssub]
-        apply pcs
-        simp_all only [Finset.mem_subtype, S, ssub]
-
-      have notin2: v ∈ val \ (coveringIntersection SF val) :=
-      by
-        rw [@Finset.mem_sdiff]
-        constructor
-        · simp_all only [Finset.mem_sdiff, Finset.mem_image, Subtype.exists, exists_and_right, exists_eq_right,
-          not_exists, true_and, exists_true_left, S, ssub]
-        · intro notinv
-          let nt := this notinv
-          rw [Finset.mem_sdiff] at notin
-          exact notin.2 nt
-
-      --tとしては、sとcover関係があるhyperedgeを持ってくる必要がある。
-
-      let ⟨Y, hY⟩ := maximal_hyperedge_subtype SF val v property.2.1 notin2
-      obtain ⟨hY1,hY2,hY3⟩ := hY
-      have :SF.sets Y ∧ v ∉ Y :=
-      by
-        simp_all only [Finset.mem_sdiff, Finset.mem_image, Subtype.exists, exists_and_right, exists_eq_right,not_exists, true_and, exists_true_left, not_false_eq_true, and_self, ssub]
-
-      have hY : Y ∈ T :=
-      by
-        dsimp [T]
-        rw [Finset.mem_filter]
-        constructor
-        · rw [Finset.mem_powerset]
-          exact SF.inc_ground Y hY1
-        · exact this
-
-      exact ⟨Y,hY⟩
-
-  --この部分ではiiが単射であることを示す。補題を利用。
-  --XとYがcover関係にあることを証明する必要がある。
-  have : ∀ X :S, cover SF (ii X) X:=
+  --SからTへの単射を作りたい。単射を作るより前に満たすべき性質を定義して、それを満たすtが非空であることをまず証明する方針。
+  --満たすべき性質とは、sの要素とtの要素は、coverの関係にあるというもの。
+  have nonemp: ∀ s :S, ∃ t:T, cover SF t s :=
   by
-    intro X
-    dsimp [cover]
-    constructor
-    · have : (ii X).val ∈ T :=
-      by
-        exact (ii X).property
-      dsimp [T] at this
-      rw [Finset.mem_filter] at this
-      exact this.2.1
-    · constructor
-      ·
-        obtain ⟨val, property⟩ := X
-        simp_all only [T]
-        simp_all only [Finset.mem_filter, Finset.mem_powerset, S, T]
-      · constructor
-        · refine Finset.ssubset_iff_subset_ne.mpr ?_
-          constructor
-          · sorry --情報が足りない？そもそもiiが分解できない。
-          · sorry --
-        · intro C hC
-          --show ↑(ii X) ⊆ C → ↑(ii X) ⊆ ↑X → C = ↑(ii X) ∨ C = ↑X
-          sorry --補題を作った方がよい？
+    intro s
+    dsimp [S] at s
+    obtain ⟨val,property⟩ := s
+    --let ssub:= val.subtype (fun x => x ∈ SF.ground)
+    rw [Finset.mem_filter] at property
+    --rw [Finset.mem_powerset] at property
+    have notin2: v ∈ val \ (coveringIntersection SF val) :=
+    by
+      exact abetype_theorem_lemma SF pc v hv val property.2.2 property.2.1
+
+    --tとしては、sとcover関係があるhyperedgeを持ってくる必要がある。
+
+    let ⟨t, hY⟩ := maximal_hyperedge_subtype SF val v property.2.1 notin2
+    obtain ⟨hY1,hY2,hY3⟩ := hY
+    have :SF.sets t ∧ v ∉ t :=
+    by
+      simp_all only [Finset.mem_sdiff, Finset.mem_image, Subtype.exists, exists_and_right, exists_eq_right,not_exists, true_and, exists_true_left, not_false_eq_true, and_self]
+
+    have hY : t ∈ T :=
+    by
+      dsimp [T]
+      rw [Finset.mem_filter]
+      constructor
+      · rw [Finset.mem_powerset]
+        exact SF.inc_ground t hY1
+      · exact this
+
+    use ⟨t,hY⟩
+
+  let ii:S → T:= fun s => Classical.choose (nonemp s)
+
+  have i_prop: ∀ s:S, cover SF (ii s) s := by
+    intro s
+    exact Classical.choose_spec (nonemp s)
 
   --単射性をいうために、
   --lemma injective_cover (SF : ClosureSystem α) [DecidablePred SF.sets]
-  -- (X1 X2 Y1 Y2 : Finset α)
-  --  (c1 : cover SF Y1 X1) (c2 : cover SF Y2 X2)
-  --  (hY : Y1 = Y2)
+  -- (X1 X2 Y : Finset α)
+  --  (c1 : cover SF Y X1) (c2 : cover SF Y X2)
   --  (h_nonempty : ((X1 \ Y1) ∩ (X2 \ Y2)).Nonempty) : X1 = X2 := by
   -- を使う。
 
+  have inj: ∀ x1 x2 :S, ii x1 = ii x2 → x1 = x2 :=
+  by
+    intro x1 x2
+    have c1: cover SF (ii x1) x1 :=
+    by
+      simp_all [T, S, ii]
+      obtain ⟨val, property⟩ := x1
+      simp_all only [Finset.mem_filter, Finset.mem_powerset, forall_true_left, T, S]
+      apply i_prop
+      simp_all only [Finset.mem_filter, Finset.mem_powerset, and_self, T, S]
+    have c2: cover SF (ii x2) x2 :=
+    by
+      simp_all [T, ii, S]
+      obtain ⟨val_1, property_1⟩ := x2
+      simp_all only [forall_true_left, T, ii, S]
+      apply i_prop
+      simp_all only [Finset.mem_filter, Finset.mem_powerset, and_self, T, ii, S]
+    intro h12
+    rw [←h12] at c2
+    let ic := injective_cover SF x1 x2 (ii x1) c1 c2
+
+
+    have nonemp:(x1.val \ (ii x1).val ∩ (x2.val \ (ii x1).val)).Nonempty:=
+    by
+      let x1p := x1.property
+      dsimp [S] at x1p
+      rw [Finset.mem_filter] at x1p
+      let x2p := x2.property
+      dsimp [S] at x2p
+      rw [Finset.mem_filter] at x2p
+      let ht1 := (ii x1).property
+      dsimp [T] at ht1
+      rw [Finset.mem_filter] at ht1
+      let ht2 := (ii x2).property
+      dsimp [T] at ht2
+      rw [Finset.mem_filter] at ht2
+
+      use v
+      rw [@Finset.mem_inter]
+      constructor
+      · rw [@Finset.mem_sdiff]
+        constructor
+        · exact x1p.2.2
+        · exact ht1.2.2
+      · rw[@Finset.mem_sdiff]
+        constructor
+        · exact x2p.2.2
+        · rw [h12]
+          exact ht2.2.2
+    simp_all [T, ii, S]
+    obtain ⟨val, property⟩ := x1
+    obtain ⟨val_1, property_1⟩ := x2
+    obtain ⟨left, right⟩ := hv
+    simp_all only [forall_true_left, Subtype.mk.injEq, T, ii, S]
+    simp_all only [T, ii, S]
+    apply ic
+    simp_all only [T, ii, S]
+
   --最後にiiの単射性を使って、vがrareであることを示す。補題を利用。
+
+  --lemma rare_and_card (SF: SetFamily α) [DecidablePred SF.sets] (v: α):
+  --SF.is_rare v ↔ (including_hyperedges SF v).card <= (deleting_hyperedges SF v).card :=
+
+  have S_eq: S = including_hyperedges SF.toSetFamily v :=
+  by
+    dsimp [S]
+    dsimp [including_hyperedges]
+    exact Finset.filter_congr (by simp [and_comm])
+  have T_eq: T = deleting_hyperedges SF.toSetFamily v :=
+  by
+    dsimp [T]
+    dsimp [deleting_hyperedges]
+    exact Finset.filter_congr (by simp [and_comm])
+
+  have: S.card <= T.card:=
+  by
+    exact Finset.card_le_card_of_injective inj
+
+  rw [S_eq,T_eq] at this
+
+  exact (rare_and_card SF.toSetFamily v).mpr this
