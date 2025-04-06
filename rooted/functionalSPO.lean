f4c2f3cc51ec22b259beccc4448ccf739969614b
Kenji Kashiwabara <kashiwa@idea.c.u-tokyo.ac.jp>
2025-04-06 23:27:04 +0900

    こめだ

--- rooted/functionalSPO.lean
+++ rooted/functionalSPO.lean
@@ -63,8 +63,13 @@ def isMaximal_spo (s: Setup_spo α) (x : Quotient s.setoid) : Prop :=
   ∀ y : Quotient s.setoid,
   s.spo.le x y → s.spo.le y x
 
+
+
 def classOf (s : Setup_spo α) (q : Quotient s.setoid) [DecidableEq (Quotient s.setoid)]  : Finset {x // x ∈ s.V} :=
   Finset.filter (fun (a : {x // x ∈ s.V}) => @Quotient.mk'' _ s.setoid a = q) s.V.attach
+--以下とほぼ同じ
+--noncomputable def eqClass_setup (s: Setup α) (x : {x : α // x ∈ s.V}) : Finset {x // x ∈ s.V} :=
+--  s.V.attach.filter (fun y => s.setoid.r x y)
 
 structure Setup_spo2 (α : Type) [Fintype α] [DecidableEq α]
   extends Setup_spo α where
@@ -107,19 +112,22 @@ def setup_setupspo (s: Setup2 α) : Setup_spo α :=
   setoid := s.setoid,
   fq := fq s
   noLoop := by
-    have : ∀ x y : Quotient s.setoid, (reach (fq s) x y → reach (fq s) y x → x = y) := by
-      intro x y hxy hyx
+    have : ∀ q1 q2 : Quotient s.setoid, (reach (fq s) q1 q2 → reach (fq s) q2 q1 → q1 = q2) := by
+      intro q1 q2 hxy hyx
       dsimp [reach] at *
       --let n := ((fq s)^[0] x)
       --let m := ((fq s)^[0] y)
-      obtain ⟨n, hn⟩ := Quotient.exists_rep x
-      obtain ⟨m, hm⟩ := Quotient.exists_rep y
+      obtain ⟨x, hx⟩ := Quotient.exists_rep q1
+      obtain ⟨y, hy⟩ := Quotient.exists_rep q2
       --nとmが同じであることはない。同じだとx=yになる。
       --xとyが違う同値類としてよい。
       --でも、x <= yと y <= xのときは、antisymmetryからx=yになる。
-      have : x ≤ y := reach_leq2 s x y hxy
-      have : y ≤ x := reach_leq2 s y x hyx
+      have : q1 ≤ q2 := reach_leq2 s q1 q2 hxy
+      have : q2 ≤ q1 := reach_leq2 s q2 q1 hyx
       apply s.po.le_antisymm
+      · subst hy hx
+        simp_all only
+      · exact this
 
     exact this,
   spo := partialOrderOfFq (fq s) (by
@@ -169,6 +177,132 @@ def setup_setupspo (s: Setup2 α) : Setup_spo α :=
 --4. 閉集合族において、パラレルな要素を持つrareな要素のtraceは、標準化次数和を上げる。
 --5. Setup2から得られる集合族の極大元に対応する同値類の大きさがすべて1であるときは、idealの集合族は、台集合上の半順序(setup_po)のideal集合族に一致する。
 
+/-
+  lemma eqClass_size_ge_two_implies_inverse
+    {α : Type} [Fintype α] [DecidableEq α]
+    (s : Setup α)
+    (x : {x // x ∈ s.V})
+    (h : 2 ≤ (eqClass_setup s x).card) :
+  ∀ y : {x // x ∈ s.V},  s.pre.le x y → s.pre.le y x := by
+-/
+/-lemma isMaximal_iff (s: Setup2 α) (a : s.V) :
+  isMaximal s.toSetup a ↔ isMaximalQ s (Quotient.mk s.setoid a) := by
+-/
+
+lemma eqClass_Class_of (s: Setup2 α) (x : {x : α // x ∈ s.V}) :
+  (eqClass_setup s.toSetup) x = classOf (setup_setupspo s) (@Quotient.mk' _ s.setoid x) := by
+  dsimp [classOf]
+  dsimp [eqClass_setup]
+  dsimp [setup_setupspo]
+  congr
+  apply funext
+  intro x
+  rename_i x_1
+  simp_all only [eq_iff_iff]
+  obtain ⟨val, property⟩ := x_1
+  obtain ⟨val_1, property_1⟩ := x
+  apply Iff.intro
+  · intro a
+    symm
+    exact Quotient.sound' a
+  · intro a
+    rw [Quotient.mk''_eq_mk] at a
+    symm
+    simp only [Quotient.mk'] at a
+    simp_all only [Quotient.eq]
+
+lemma eqClass_Class_of2 (s: Setup2 α) (q : Quotient s.setoid) :
+  eqClass_setup s.toSetup (Quotient.out q) = (classOf (setup_setupspo s) q) :=
+by
+  rw [eqClass_Class_of]
+  congr
+  simp [Quotient.mk']
+
+lemma spole_iff_po (s: Setup2 α) (x y : Quotient s.setoid) :
+  s.po.le x y ↔ (setup_setupspo s).spo.le x y := by
+  dsimp [setup_setupspo]
+  dsimp [partialOrderOfFq]
+  dsimp [reach]
+  apply Iff.intro
+  · intro h
+    let fql := fq_lemma s x y h
+    obtain ⟨n, hn⟩ := fql
+    use n
+    exact hn.symm
+  · intro h
+    obtain ⟨n, hn⟩ := h
+    have :(∃ n, y = (fq s)^[n] x) := by
+      use n
+      subst hn
+      simp_all only
+    let fql := fq_lemma_rev s x y this
+    exact fql
+
+lemma isMaximal_spo_iff (s: Setup2 α) (q : Quotient s.setoid) :
+  isMaximal_spo (setup_setupspo s) q ↔
+  isMaximalQ s q :=
+by
+  dsimp [isMaximal_spo]
+  dsimp [isMaximalQ]
+  dsimp [setup_setupspo]
+  apply Iff.intro
+  · intro h
+    intro y
+    let hy := h y
+    rw [spole_iff_po]
+    rw [spole_iff_po]
+    exact hy
+  · intro h
+    intro y
+    let hy := h y
+    rw [spole_iff_po] at hy
+    rw [spole_iff_po] at hy
+    exact hy
+
+lemma eqClass_Maximal (s: Setup2 α) (q : Quotient s.setoid) :
+  (classOf (setup_setupspo s) q).card ≥ 2 → isMaximalQ s q  := by
+  intro h
+  rw [←eqClass_Class_of2] at h
+  dsimp [isMaximalQ]
+  let ecs := eqClass_size_ge_two_implies_inverse s.toSetup (Quotient.out q) h
+  obtain ⟨x, hx⟩ := Quotient.exists_rep q
+  intro q2
+  specialize ecs x
+  let imi := isMaximal_iff s x
+  rw [hx] at imi
+  dsimp [isMaximal] at imi
+  dsimp [isMaximalQ] at imi
+
+
+
+
+
+lemma eqClass_Maximal_spo (s: Setup_spo α)  (q : Quotient s.setoid) :
+  (classOf s q).card = 1 → ¬ isMaximal_spo s q := by
+  intro h
+  dsimp [isMaximal_spo]
+  rw [←eqClass_Class_of2 s] at h
+
+  --dsimp [classOf] at h
+  rw [Finset.card_eq_one] at h
+  --intro hm
+  simp_all only [Subtype.exists]
+
+
+
+
+
+
+
+  apply Iff.intro
+  · intro a
+    exact Quotient.sound' a
+  · intro a
+    rw [Quotient.mk''_eq_mk] at a
+    symm
+    simp only [Quotient.mk'] at a
+    simp_all only [Quotient.eq]
+
 def setup2_induces_spo (s : Setup2 α) : Setup_spo2 α :=
 {
   V := s.V,
@@ -185,6 +319,15 @@ def setup2_induces_spo (s : Setup2 α) : Setup_spo2 α :=
       intro h
       apply hq
       exact h
+    --hqにqがmaximalでないという条件が入っていて、
+    --同値類の大きさが1であることを示す。
+
+    let eqc := eqClass_Class_of s
+
+
+
+
+
     dsimp [singleton_if_not_maximal] at this
     have : (classOf (setup_setupspo s) q).card = 1 := by
       apply this q
